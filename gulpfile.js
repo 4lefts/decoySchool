9190b236a3e71e2849edcc41ca88e23e6337b58f
4lefts <5t3ph3n.ball@gmail.com>
2017-07-18 16:14:53 +0100

    tweaked gulpfile again

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,16 @@
-
-		// gulp and plugins
+			// gulp and plugins
 const gulp = require('gulp'),
-			// sass = require('gulp-sass'),
-			// prefix = require('gulp-autoprefixer'),
+			sass = require('gulp-sass'),
+			prefix = require('gulp-autoprefixer'),
 			// metalsmith and plugins
-			metalsmith = require('metalsmith'),
+			metalsmith = require('gulp-metalsmith'),
 			markdown = require('metalsmith-markdown'),
 			layouts = require('metalsmith-layouts'),
 			collections = require('metalsmith-collections'),
 			permalinks = require('metalsmith-permalinks'),
-			sass = require('metalsmith-sass'),
-			prefixer = require('metalsmith-autoprefixer'),
-			ignore = require('metalsmith-ignore'),
 			browserSync = require('browser-sync').create() //create method gives unique browsersync instance and allows multiple servers
 
-//stuff for date (used in templates)
+//stuff for date
 var d = new Date()
 var year = d.getFullYear()
 
@@ -22,59 +18,64 @@ function errorLog(error){
 	console.error(error.message)
 }
 
-gulp.task('buildSite', () => {
-	return metalsmith(__dirname)
-		.metadata({
-			site: {
-				title: "Decoy School",
-				bannerImage: "learning",
+gulp.task('metalsmith', function(){
+	return gulp.src('src/**')
+		.pipe(metalsmith({
+			metadata: {
+				site: {
+					title: "Decoy School",
+					bannerImage: "learning",
+				},
+				thisYear: year,
 			},
-			thisYear: year,
-		})
-		.source('./src')
-		.destination('./build')
-		.clean(true)
-		.use(sass({
-			outputDir: function(originalPath){
-				return originalPath.replace('sass', 'styles')
-			},
-			outputStyle: 'expanded',
+			use: [
+				collections({
+					pages: {
+						pattern: 'pages/*.md',
+						sortBy: 'order',
+					},
+					yearGroups: {
+						pattern: 'yearGroups/*.md',
+					},
+					footerLogos: {
+						pattern: 'images/footerLogos/*.png'
+					},
+					extra: {
+						pattern: 'extra/*.md',
+					},
+				}),
+				markdown({
+					"smartypants": true,
+					"gfm": true,
+					"tables": true,
+				}),
+				permalinks({
+	 				pattern: ":title",
+	 				relative: false
+				}),
+				layouts({
+					'engine': 'jade'
+				}),
+			]
 		}))
-		.use(prefixer())
-		.use(collections({
-			pages: {
-				pattern: 'pages/*.md',
-				sortBy: 'order',
-			},
-			yearGroups: {
-				pattern: 'yearGroups/*.md',
-			},
-			footerLogos: {
-				pattern: 'images/footerLogos/*.png'
-			},
-			extra: {
-				pattern: 'extra/*.md',
-			},
-		}))
-		.use(ignore(['sass/**']))
-		.use(markdown({
-			"smartypants": true,
-			"gfm": true,
-			"tables": true,
-		}))
-		.use(permalinks({
-			pattern: ":title",
-			relative: false
-		}))
-		.use(layouts({
-			'engine': 'jade'
-		}))
-		.build((err) => {
-			if(err) throw err
-		})
+		.on('error', errorLog)
+		.pipe(gulp.dest('build'))
 })
 
-gulp.task('serve', () => {
+gulp.task('buildStyles', function(){
+	return gulp.src('sass/**/*')
+		.pipe(sass({
+			outputStyle: 'compressed',
+			sourceMaps: false,
+		}))
+		.on('error', errorLog)
+		.pipe(prefix({
+			browsers: ['last 2 versions'],
+		}))
+		.pipe(gulp.dest('src/styles'))
+})
+
+gulp.task('serve', function(){
 	browserSync.init({
 		server: {
 			baseDir: 'build',
@@ -82,12 +83,16 @@ gulp.task('serve', () => {
 	})
 })
 
-gulp.task('refresh', ['buildSite'], () => {
-	setTimeout(function(){
+gulp.task('refresh', ['metalsmith'], function(){
+	setTimeout(() => {
 		return browserSync.reload()
 	}, 1500)
 })
 
-gulp.task('watch', () => gulp.watch(['src/**', 'layouts/**'], ['refresh']))
+gulp.task('watch', function(){
+	gulp.watch('layouts/**/*', ['buildStyles', 'refresh'])
+	gulp.watch('src/**/*', ['buildStyles', 'refresh'])
+	gulp.watch('sass/**/*', ['buildStyles', 'refresh'])
+})
 
-gulp.task('default', ['buildSite', 'serve', 'watch'])
+gulp.task('default', ['buildStyles', 'metalsmith', 'serve', 'watch'])
